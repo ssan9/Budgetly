3a40e15afe82f12e75cda79501c8f4371fe1b91c
ssan9 <perrsy@gmail.com>
2017-12-26 11:03:46 -0500

    modified files

---
+++ expense.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#edit').hide();
+  });
+
+$(function() {
+    $('.active a').on('click', function(e) {
+  event.preventDefault();
+    $('#add').show();
+    $('#edit').hide();
+  });
+});
+
+ $(function() {
+    $('.inactive a').on('click', function(e) {
+      event.preventDefault();
+      $('#add').hide().removeClass('.active');
+      $('#edit').show().addClass('.active');
+    });
+ });
--- signup.js
+++ signup.js
@@ -43,22 +43,82 @@ $(function() {
 
 });
 
-$(function() {
+// $(function() {
 
-  $('.tab a').on('click', function (e) {
+//   $('.tab a').on('click', function (e) {
 
-    e.preventDefault();
+//     e.preventDefault();
 
-    $(this).parent().addClass('active');
-    console.log($(this).parent());
-    $(this).parent().siblings().removeClass('active');
+//     $(this).parent().addClass('active');
+//     console.log($(this).parent());
+//     $(this).parent().siblings().removeClass('active');
 
-    target = $(this).attr('href');
+//     target = $(this).attr('href');
 
-    $('.content > div').not(target).hide();
+//     $('.content > div').not(target).hide();
 
-    $(target).fadeIn(600);
+//     $(target).fadeIn(600);
 
+//     $(this).fadeIn(600);
+
+//   });
+
+// });
+
+// $(function() {
+
+//   $('.active a').on('click', function(e) {
+//     e.preventDefault();
+//     $('#signup').fadeIn();
+//     $('#login').fadeOut(function() {
+//       $(this).removeClass('active');
+//     });
+//   });
+
+
+//   $('.inactive a').on('click', function(e) {
+//     e.preventDefault();
+//     $('#signup').fadeOut(function() {
+//       $(this).removeClass('active');
+//     });
+//     $('#login').fadeIn(function() {
+//      $(this).addClass('active');
+//     });
+//   });
+// });
+
+
+ // $(function() {
+ //    $('.inactive a').on('click', function(e) {
+ //      event.preventDefault();
+ //      $('#signup').hide();
+ //      $('#login').show();
+ //    });
+ // });
+
+ $(function() {
+
+  $('.active a').on('click', function(e) {
+  event.preventDefault();
+    $('#signup').show();
+    $('#login').hide();
   });
-
 });
+
+ $(function() {
+    $('.inactive a').on('click', function(e) {
+      event.preventDefault();
+      $('#signup').hide().removeClass('.active');
+      $('#login').show().addClass('.active');
+    });
+ });
+    
+  
+
+ // $(function() {
+ //  $('.inactive a').on('click', function(e) {
+ //    e.preventDefault();
+ //    $('#signup').hide();
+ //    $('#login').show();
+ //  })
+ // })
