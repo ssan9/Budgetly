b383ebb70fc9c90181e0dc438f4171d899937089
ssan9 <perrsy@gmail.com>
2017-12-28 11:48:37 -0500

    added post request in expense.js

--- public/expense-history.js
+++ public/expense-history.js
@@ -9,7 +9,7 @@ $.ajax
   {
      console.log(data);
      const expenseHtml = data.map(function(entry) {
-     	return `<h1>${entry.description}</h1>`;
+     	return `<h2>${entry.date}<span>$${entry.amount}</span><span>${entry.category}</span><span>${entry.description}</span></h2>`;
      })
      $("#expense-data").html(expenseHtml);
   }
--- public/expense.js
+++ public/expense.js
@@ -1,19 +1,83 @@
 $(function() {
-    $('#edit').hide();
-  });
 
-$(function() {
-    $('.active a').on('click', function(e) {
-  event.preventDefault();
-    $('#add').show();
-    $('#edit').hide();
+  $('.container').find('input').on('keyup blur focus', function (e) {
+
+    var $this = $(this),
+    label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+      if ($this.val() === '') {
+        label.removeClass('active highlight');
+      } 
+      else {
+        label.addClass('active highlight');
+      }
+    }
+    else if (e.type === 'blur') {
+      if( $this.val() === '' ) {
+        label.removeClass('active highlight'); 
+      } 
+      else {
+        label.removeClass('highlight');   
+      }  
+    } 
+    else if (e.type === 'focus') {
+      if( $this.val() === '' ) {
+        label.removeClass('highlight'); 
+      } 
+      else if( $this.val() !== '' ) {
+        label.addClass('highlight');
+      }
+    }
   });
 });
 
- $(function() {
-    $('.inactive a').on('click', function(e) {
-      event.preventDefault();
-      $('#add').hide().removeClass('.active');
-      $('#edit').show().addClass('.active');
+$(function() {
+  $('#edit').hide();
+
+
+
+  $('.active a').on('click', function(e) {
+    event.preventDefault();
+    $('#add').show();
+    $('#edit').hide();
+  });
+
+  $('.inactive a').on('click', function(e) {
+    event.preventDefault();
+    $('#add').hide().removeClass('.active');
+    $('#edit').show().addClass('.active');
+  });
+
+
+  
+  $("#save").click(function(e) {
+    e.preventDefault();
+    let newExpense = {
+      date: $("#date").val(),
+      amount: $(".amount").val(),
+      category: $("#category").val(),
+      description: $(".description").val()
+    }
+    var url = "/data";
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: JSON.stringify(newExpense),
+      contentType: "application/json; charset=utf-8",
+
+      success: function(data)
+      {
+        console.log(data);
+        window.location="/expense-history.html"
+        $( "#expense-data" ).html(data);
+      },
+          failure: function(errMsg) {
+        alert(errMsg);
+    }
+
     });
- });
+  })
+})
+
