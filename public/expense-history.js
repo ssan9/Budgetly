5e44bb9407a2d35ba1deae2c63c55109f0304700
Sebastian Bastidas <sbastidasr@gmail.com>
2018-01-08 11:57:46 -0500

    refactor expenses / signup

--- public/expense-history.js
+++ public/expense-history.js
@@ -1,28 +1,41 @@
 $(function() {
 	var url = "/api/expenses";
-
 	$.ajax({
 		type: "GET",
 		url: url,
 		success: function(data)
 		{
-			console.log(data);
-			const expenseHtml = data.map(function(entry) {
-				var strDate=moment(entry.date).format('MMMM Do, YYYY');
+			showExpenses(data)
+		}
+	});
 
-				return `<div class="expense-details"><div id="date">${strDate}</div><div id="amount">$${entry.amount}</div><div id="category">${entry.category}</div><div id="description">${entry.description}</div>
-				<button id="edit">Edit</button><button id="delete">Delete</button></div>`;
-			})
-			$("#expense-data").html(expenseHtml);
+	$(".month").on("change", function(){
+		var selected = $(this).val();
+		if(selected) {
+			makeAjaxRequest(selected);
 		}
 	});
 
 
+function showExpenses(data){
+	const expenseHtml = data.map(function(entry) {
+		var strDate=moment(entry.date).format('MMMM Do, YYYY');
+		return `<div class="expense-details">
+							<div id="date">${strDate}</div>
+							<div id="amount">$${entry.amount}</div>
+							<div id="category">${entry.category}</div>
+							<div id="description">${entry.description}</div>
+							<button id="edit">Edit</button>
+							<button id="delete">Delete</button>
+						</div>`;
+	})
+	$("#expense-data").html(expenseHtml);
+}
 
 	// const month=$(".switch-month").val();
 	//   filter.month
 	function makeAjaxRequest(selectedMonth){
-		// var strDate=moment(selectedMonth).format('MMMM, YYYY');
+		var strMonth=moment(selectedMonth).format('MMMM');
 		var strDateMonth=moment(selectedMonth).format('MM');
 		var strDateYear=moment(selectedMonth).format('YYYY');
 		// const isoDate=moment(strDate).format()
@@ -31,18 +44,13 @@ $(function() {
 			type: "GET",
 			url: url + "/" + strDateMonth + "/" + strDateYear,
 			success: function(data) {
-				console.log(data);
-				$("#monthly-record").html(`${data}`);
+				showExpenses(data)
+				$("#monthly-record").html(strMonth);
 			}
 		});
 	}
 
-	$(".month").on("change", function(){
-		var selected = $(this).val();
-		if(selected) {
-			makeAjaxRequest(selected);
-		}
-	});
+
 
 })
 
--- public/signup.js
+++ public/signup.js
@@ -1,164 +1,19 @@
-// $(function() {
-
-//   $('.container').find('input').on('keyup blur focus', function (e) {
-
-//     var $this = $(this),
-//     label = $this.prev('label');
-
-//     if (e.type === 'keyup') {
-
-//       if ($this.val() === '') {
-//         label.removeClass('active highlight');
-//       } 
-
-//       else {
-//         label.addClass('active highlight');
-//       }
-//     }
-
-//     else if (e.type === 'blur') {
-
-//       if( $this.val() === '' ) {
-//         label.removeClass('active highlight'); 
-//       } 
-
-//       else {
-//         label.removeClass('highlight');   
-//       }  
-
-//     } 
-
-//     else if (e.type === 'focus') {
-
-//       if( $this.val() === '' ) {
-//         label.removeClass('highlight'); 
-//       } 
-
-//       else if( $this.val() !== '' ) {
-//         label.addClass('highlight');
-//       }
-//     }
-
-//   });
-
-// });
-
-// $(function() {
-
-//   $('.tab a').on('click', function (e) {
-
-//     e.preventDefault();
-
-//     $(this).parent().addClass('active');
-//     console.log($(this).parent());
-//     $(this).parent().siblings().removeClass('active');
-
-//     target = $(this).attr('href');
-
-//     $('.content > div').not(target).hide();
-
-//     $(target).fadeIn(600);
-
-//     $(this).fadeIn(600);
-
-//   });
-
-// });
-
-// $(function() {
-
-//   $('.active a').on('click', function(e) {
-//     e.preventDefault();
-//     $('#signup').fadeIn();
-//     $('#login').fadeOut(function() {
-//       $(this).removeClass('active');
-//     });
-//   });
-
-
-//   $('.inactive a').on('click', function(e) {
-//     e.preventDefault();
-//     $('#signup').fadeOut(function() {
-//       $(this).removeClass('active');
-//     });
-//     $('#login').fadeIn(function() {
-//      $(this).addClass('active');
-//     });
-//   });
-// });
-
-
- // $(function() {
- //    $('.inactive a').on('click', function(e) {
- //      event.preventDefault();
- //      $('#signup').hide();
- //      $('#login').show();
- //    });
- // });
-
-//  $(function() {
-
-//   $('.active a').on('click', function(e) {
-//   event.preventDefault();
-//     $('#signup').show();
-//     $('#login').hide();
-//   });
-// // });
-
-
-//     $('.inactive a').on('click', function(e) {
-//       event.preventDefault();
-//       $('#signup').hide().removeClass('.active');
-//       $('#login').show().addClass('.active');
-//     });
-
-//  });
-    
-  
-
- // $(function() {
- //  $('.inactive a').on('click', function(e) {
- //    e.preventDefault();
- //    $('#signup').hide();
- //    $('#login').show();
- //  })
- // })
-
- $(function() {
+$(function() {
   $('#login').hide();
-  // $('#su a').addClass('active');
   $('#su a').css('background-color', '#24896d');
-  console.log($('#login'));
-  console.log($('#su'));
-
 
   $('.active a').on('click', function(e) {
     e.preventDefault();
-    console.log($('li'));
-    // alert("active");
-    // $('#su').removeClass('inactive').addClass('active');
-
     $('#signup-tab').show();
-      $('#su a').css('background-color', '#24896d');
-
-    console.log($('#signup-tab'));
-
+    $('#su a').css('background-color', '#24896d');
     $('#login').hide();
   });
 
   $('.inactive a').on('click', function(e) {
     e.preventDefault();
-        // alert("inactive");
-
-    // $('lg').removeClass('inactive').addClass('active');
-    // toggleClass('.active');
-    $('#su a').css('background-color' , 'rgba(160, 179, 176, 0.25)');
-    console.log($('#signup-tab'));
+    $('#su a').css('background-color', 'rgba(160, 179, 176, 0.25)');
     $('#signup-tab').hide();
     $('#login').show();
   });
 
-  
-});  
-
- 
+});
