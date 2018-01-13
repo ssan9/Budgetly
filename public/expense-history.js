4429427ce65c9e96bc7c86cc0b1c6a20ba423289
ssan9 <perrsy@gmail.com>
2018-01-12 20:33:32 -0500

    added table to expense-history

--- public/expense-history.js
+++ public/expense-history.js
@@ -19,58 +19,86 @@ $(function() {
 		}
 	});
 
+	function renderForm(params) {
+		const expenseHtml = `<div class="expense-details" params-id="${params.id}">
+		<input type="date" name="date" id="date" class="expense-form">
+		<input type="amount" name="amount" id="amount" placeholder="Amount" class="expense-form">
+		<select name="category" id='category' class="expense-form" required>
+		<option value="0">Category</option>
+		<option value="Gas">Gas</option>
+		<option value="Entertainment">Entertainment</option>
+		<option value="Home Decor">Home Decor</option>
+		<option value="Restaurants and Bar">Restaurants and Bar</option>
+		<option value="Travel">Travel</option>
+		<option value="Grocery">Grocery</option>
+		<option value="Other">Other</option>
+		</select>
+		<input type="description" name="description" id="description" placeholder="Description" class="expense-form">
+		<button class="save">Save</button>
+		<button class="cancel">Cancel</button>
+		</div>`;
+		$(params).html(expenseHtml);
+	}
+
+	
 
 	function showExpenses(data){
 		const expenseHtml = data.map(function(entry) {
 			var strDate=moment(entry.date).format('MMMM Do, YYYY');
 			return `<div class="expense-details" data-id="${entry.id}">
-			<div class="date">${strDate}</div>
-			<div class="amount">$${entry.amount}</div>
-			<div class="category">${entry.category}</div>
-			<div class="description">${entry.description}</div>
-			<button class="edit">Edit</button>
-			<button class="delete">Delete</button>
-			</div>`;
-		})
+	  					<table>
+	    					<tr>
+						      <td class="date data data-js" data-html-label="date">${strDate}</td>
+						      <td class="amount data data-js" data-html-label="amount">$${entry.amount}</td>
+						      <td class="category data data-js" data-html-label="category">${entry.category}</td>
+						      <td class="description data data-js" data-html-label="description">${entry.description}</td>
+						      <td class="edit buttons" data-html-label="edit"><button class="edit">Edit</button></td>
+							  <td class="delete buttons" data-html-label="delete"><button class="delete">Delete</button></td>
+
+							</tr>
+	    				</table>	
+      				</div>`
+
+							
+	})
 
 		$("#expense-data").html(expenseHtml);
-
-		$(".edit").click(e => {
-			e.preventDefault();
-			var button=e.currentTarget;
-			var editRowID=$(button).parent('.expense-details').attr('data-id');
-			$(".black-cover").removeClass("hidden");
-			$(".modal").attr("data-id", editRowID)
-			editRow.html(newForm);
-		})
-
-
-		$(".save").click(e=> {
-			e.preventDefault();
-			var expenseId = $(e.currentTarget).parent('.modal').attr('data-id');
-			var params={
-				strDate: $(".modal").find(".date").text(),
-				amount: $(".modal").find(".amount").text(),
-				category: $(".modal").find(".category").text(),
-				description: $(".modal").find(".description").text(),
-				id:expenseId
-			};
-
-			$.ajax({
-				type: "PUT",
-				url: url + "/" + expenseId,
-				success: function(data) {
-					getData();
-				},
-				error: function(err) {
-					console.log(err);
-				}
-			})
-		})
-
-
-
-
+		
+		
+		$(".edit").click(e => {		  		
+ 			
+  			e.preventDefault();		  			
+  			var button=e.currentTarget;		  					 			
+ 			var editRowID=$(button).parent('.expense-details').attr('data-id');
+ 			$(".black-cover").removeClass("hidden");
+ 			$(".modal").attr("data-id", editRowID)
+ 			editRow.html(newForm);
+ 		})
+ 
+ 
+ 		// $(".save").click(e=> {
+ 		// 	e.preventDefault();
+			// var expenseId = $(e.currentTarget).parent('.modal').attr('data-id');		 
+  	// 		var params={		 				
+ 		// 		strDate: $(".modal").find(".date").text(),		 				
+ 		// 		amount: $(".modal").find(".amount").text()
+ 		// 		category: $(".modal").find(".category").text(),	 				
+ 		// 		description: $(".modal").find(".description").text(),
+ 		// 		id:expenseId
+  	// 		};		  			
+  			
+  		  
+ 	 // 		$.ajax({
+ 		// 		type: "PUT",
+ 		// 		url: url + "/" + expenseId,
+ 		// 		success: function(data) {
+ 		// 			getData();
+ 		// 		},
+ 		// 		error: function(err) {
+ 		// 			console.log(err);
+ 		// 		}
+ 		// 	})
+ 		// })
 	}
 
 
@@ -90,38 +118,7 @@ $(function() {
 
 	}
 
-	// $(".edit").click(e) => {
-	// 		e.preventDefault();
-	// 		var button=e.currentTarget;
-	// 		var editRow=$(button).parent("expense-details");
-	// 		var params={
-	// 			strDate: $(editRow).find(".date").text(),
-	// 			amount: $(editRow).find(".amount").text(),
-	// 			category: $(editRow).find(".category").text(),
-	// 			description: $(editRow).find(".description").text()
-	// 		};
-
-	// 		// var newForm=renderForm(params);
-
-	// 		// editRow.html(newForm);
-
-	// 		$(".save").click(e)=> {
-	// 			e.preventDefault();
-	// 			var expenseId = $(e.currentTarget).parent('.expense-details').attr('data-id');
-
-	// 			$.ajax({
-	// 				type: "PUT",
-	// 				url: url + "/" + expenseId,
-	// 				success: function(data) {
-	// 					getData();
-	// 				},
-	// 				error: function(err) {
-	// 					console.log(err);
-	// 				}
-	// 			})
-	// 		}
-	// 	}
-
+	
 
 	$("#expense-data").on("click", ".delete", function(e) {
 		e.preventDefault();
@@ -131,7 +128,7 @@ $(function() {
 			url: url + "/" + expenseId,
 			success: function(data) {
 				getData();
-			},
+			}, 
 			error: function(err) {
 				console.log(err);
 			}
@@ -145,31 +142,3 @@ $(function() {
 
 
 
-// $("#edit").click(function(e) { //taking values from input fields and also setting parameters
-// 	e.preventDefault();
-// 	console.log('${#edit}');
-// 	let update = {
-// 		date: $("#date").val(),
-// 		amount: $(".amount").val(),
-// 		category: $("#category").val(),
-// 		description: $(".description").val()
-// 	}
-// 	var url = "/api/expenses/";
-
-// 	$.ajax({
-// 		type: "PUT",
-// 		url: url,
-// 		data: JSON.stringify(update),
-// 		contentType: "application/json; charset=utf-8",
-
-// 		success: function(data)
-// 		{
-// 			console.log(data);
-// 			// window.location="/expense-history.html"
-// 			$( "#expense-data" ).html(data);
-// 		},
-// 		failure: function(errMsg) {
-// 			alert(errMsg);
-// 		}
-// 	});
-// })
