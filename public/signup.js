4c6639027b13a371e739dcefbb6073fbd596d59b
ssan9 <perrsy@gmail.com>
2018-06-13 15:17:25 -0400

    empty fields

--- public/signup.js
+++ public/signup.js
@@ -53,8 +53,6 @@ $(function() {
          $("#su a") .css("background-color", "rgba(160, 179, 176, 0.25)");
         $("#login").show()
         $("#lg a").css("background-color", "#307c68");
-        $("#username").val("");
-        $("#password").val("");
       },
       error: function(errMsg) {
         console.log(errMsg);
@@ -67,7 +65,9 @@ $(function() {
     e.preventDefault();
     let loginInfo = {
       username: $("#username-login").val(),
-      password: $("#password-login").val()
+      password: $("#password-login").val(),
+      $("#username-login").val(""),
+      $("#password-login").val("")
     };
 
     var url = "/api/auth/login";
